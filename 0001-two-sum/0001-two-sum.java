774d51614a5023013b4fc964c51d4adc1e33e307
brijshyam <108238421+brijshyam@users.noreply.github.com>
2022-10-26 20:14:03 +0530

    Time: 5 ms (78.27%), Space: 45.3 MB (44.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -15,6 +15,22 @@ class Solution {
         hm.put(nums[i], i);
         
     }
-    return res;
-}
+        
+    //TWO POINTER APPROACH 
+        // Arrays.sort(nums);
+        // int left=0;
+        // int right= nums.length-1;
+        // while(left< right){
+        //     int sum =nums[left]+nums[right];
+        //     if(sum==target){
+        //         res[0]=left;
+        //         res[1]=right;
+        //         return res;
+        //     }else if(sum < target){
+        //         left++;
+        //     }else
+        //         right--;
+        // }
+        return res;
+    }
 }
