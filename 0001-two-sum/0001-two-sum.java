50d6adec0e9e0786f1a77775ca8f143075de4131
brijshyam <108238421+brijshyam@users.noreply.github.com>
2022-10-26 20:29:29 +0530

    Time: 5 ms (78.27%), Space: 45.9 MB (23.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,36 +1,19 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-    int[] res = new int[2];
-    
-    Map<Integer, Integer> hm = new HashMap<>();
-    hm.put(nums[0],0);
-        
-    for(int i =1; i < nums.length; i++ ){
-        int temp = target - nums[i];
-        if(hm.containsKey(temp)){
-            res[1]=i;
-            res[0]= hm.get(temp);
-            break;
+        Map<Integer, Integer> hm = new HashMap<>();
+        for(int i =0; i < nums.length; i++ ){
+            int temp = target - nums[i];
+            if(hm.containsKey(temp)){
+                 int []  res = {hm.get(temp), i} ;
+                return res;
+
+            }
+            hm.put(nums[i], i);
+
         }
-        hm.put(nums[i], i);
+        return null;
+    
         
-    }
-        
-    //TWO POINTER APPROACH 
-        // Arrays.sort(nums);
-        // int left=0;
-        // int right= nums.length-1;
-        // while(left< right){
-        //     int sum =nums[left]+nums[right];
-        //     if(sum==target){
-        //         res[0]=left;
-        //         res[1]=right;
-        //         return res;
-        //     }else if(sum < target){
-        //         left++;
-        //     }else
-        //         right--;
-        // }
-        return res;
+   
     }
 }
