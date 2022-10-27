29594d7c1efa1db75ec05c1b64c650482220fcdc
brijshyam <108238421+brijshyam@users.noreply.github.com>
2022-10-27 11:11:09 +0530

    Time: 3 ms (86.09%), Space: 42.4 MB (91.97%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -4,7 +4,6 @@ class Solution {
         int len1 = nums1.length; 
         int len2 = nums2.length;
         ArrayList<Integer> list = new ArrayList<>();
-        if(len1> len2){
             for(int i : nums1){
                 hm.put(i, hm.getOrDefault(i, 0)+1);
             }
@@ -16,19 +15,17 @@ class Solution {
                         hm.remove(i);
                 }
             }
-        }else if(len2>=len1){
-            for(int i : nums2){
-                hm.put(i, hm.getOrDefault(i, 0)+1);
-            }
-            for(int i: nums1){
-                if(hm.containsKey(i)){
-                    list.add(i);
-                    hm.put(i, hm.get(i)-1);
-                    if(hm.get(i)==0)
-                        hm.remove(i);
-                }
-            }
-        }
+            // for(int i : nums2){
+            //     hm.put(i, hm.getOrDefault(i, 0)+1);
+            // }
+            // for(int i: nums1){
+            //     if(hm.containsKey(i)){
+            //         list.add(i);
+            //         hm.put(i, hm.get(i)-1);
+            //         if(hm.get(i)==0)
+            //             hm.remove(i);
+            //     }
+            // }
         int [] res = new int[list.size()];
         for(int i =0; i<list.size(); i++){
             res[i]= list.get(i);
