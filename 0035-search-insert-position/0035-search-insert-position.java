8d7c07fb44eb1f71c489d3658994f722cef336d9
brijshyam <108238421+brijshyam@users.noreply.github.com>
2022-12-09 11:41:00 +0530

    Time: 0 ms (100.00%), Space: 44.6 MB (6.19%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -3,28 +3,31 @@ class Solution {
         return binarySearch(nums, 0, nums.length-1, target);
     }
     public int binarySearch(int [] arr, int first, int last, int target){
-        // if(first>last){
-        //     return first ;
-        // }
-        // int mid =last-(last-first)/2;
-        // if(target>arr[mid])
-        //     return binarySearch(arr, mid+1, last, target);
-        // if(target<arr[mid])
-        //     return binarySearch(arr, first, mid-1, target);
-        // return mid ;
-        
-        while(first<=last ){
-            int mid = last -(last-first )/2;
-            if(mid<arr.length && target> arr[mid]){
-                first = mid+1; 
-                
-            }else if(target<arr[mid]){
-                last = mid-1;
-            }else if(target == arr[mid]){
-                return mid ;
-            }
+        if(first>last){
+            return first ;
         }
-        return first;
+        int mid =last-(last-first)/2;
+        if(target>arr[mid])
+            return binarySearch(arr, mid+1, last, target);
+        if(target<arr[mid])
+            return binarySearch(arr, first, mid-1, target);
+        return mid ;
+        
+        
+        
+        
+//         while(first<=last ){
+//             int mid = last -(last-first )/2;
+//             if(mid<arr.length && target> arr[mid]){
+//                 first = mid+1; 
+                
+//             }else if(target<arr[mid]){
+//                 last = mid-1;
+//             }else if(target == arr[mid]){
+//                 return mid ;
+//             }
+//         }
+//         return first;
         
        
     }
