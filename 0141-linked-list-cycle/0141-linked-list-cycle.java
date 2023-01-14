db5248d2ca1f5a6e15adf145afd8239daffd34c4
brijshyam <108238421+brijshyam@users.noreply.github.com>
2023-01-15 03:08:32 +0530

    Time: 0 ms (100.00%), Space: 44 MB (50.86%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,15 +11,18 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        if(head == null ) return false ;
-        ListNode slow = head; 
+        if(head == null) return false; 
+        
+        //tortoise and turtle method
+        ListNode slow = head ;
         ListNode fast = head; 
-        while(fast!=null && fast.next!=null){
-            slow=slow.next; 
-            fast = fast.next.next; 
+        while(fast != null && fast.next!=null){
+            slow= slow.next;
+            fast= fast.next.next; 
             if(slow== fast )
-                    return true;
+                return true; 
         }
-        return false;
+        
+        return false ; 
     }
 }
